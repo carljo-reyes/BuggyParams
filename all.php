ee8d6eac93c2f8909299a6c6b4342f91842cf06d
romyowski <30599372+romyowski@users.noreply.github.com>
2018-06-06 15:43:20 +0800

    Add files via upload

---
+++ all.php
@@ -0,0 +1,21 @@
+<style media="screen">
+
+	* {
+		margin-bottom: 10px;
+	}
+
+	body {
+		background-color: #B17BC4;
+	}
+</style>
+
+<body>
+
+	<h1>This is All!</h1>
+	<a href="filt.php">Go to Filt</a>
+
+
+	<script type="text/javascript" src="https://c0acy518.caspio.com/dp/92656000b5d11be9dfc947839fbd/emb"></script>
+	<div class="cxkg"><a href="https://c0acy518.caspio.com/dp/92656000b5d11be9dfc947839fbd?cbResetParam=1">Click here</a> to load this Caspio <a href="http://www.caspio.com" target="_blank" title="Cloud Database">Cloud Database</a></div>
+
+</body>
---
+++ filt.php
@@ -0,0 +1,19 @@
+<style media="screen">
+
+	* {
+		margin-bottom: 10px;
+	}
+
+	body {
+		background-color: #97A49A;
+	}
+</style>
+<body>
+
+<h1>This is Filt!</h1>
+<a href="all.php">Go to All</a>
+
+<script type="text/javascript" src="https://c0acy518.caspio.com/dp/92656000ddabfe528cb64331a550/emb"></script>
+<div class="cxkg"><a href="https://c0acy518.caspio.com/dp/92656000ddabfe528cb64331a550?cbResetParam=1">Click here</a> to load this Caspio <a href="http://www.caspio.com" target="_blank" title="Cloud Database">Cloud Database</a></div>
+
+</body>
